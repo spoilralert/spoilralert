a238dba5f7a88780970ccfe025218cbf4a648eb6
Linda-404 <Linda-404@users.noreply.github.com>
2022-05-04 13:24:49 +0200

    add fontawsome icons

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,12 @@ import Paragraph from "../components/paragraph";
 import Search from "../components/search";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import {
+  faSearch,
+  faPlus,
+  faUserNinja,
+  faStar,
+} from "@fortawesome/free-solid-svg-icons";
 
 // import Head from 'next/head'
 // import Image from 'next/image'
@@ -31,10 +36,6 @@ export default function Home() {
         <Search />
       </section>
 
-      {/* <section>
-        <Search />
-      </section> */}
-
       <section>
         <div className="intro">
           <Heading title="Long story short" />
@@ -52,21 +53,50 @@ export default function Home() {
       </section>
       <section className="description-container">
         <h2>how does it work?</h2>
-        <div className="description">
-          <h4>Finding spoilers:</h4>
-          <Paragraph text="-search for movie title to get details, then you can choose if you want to see the spoilers or not ;)" />
+        <div className="description-wrapper">
+          <FontAwesomeIcon
+            icon={faSearch}
+            style={{ fontSize: 50 }}
+            className="icon"
+          />
+          <div className="description">
+            <h4>Finding spoilers:</h4>
+            <Paragraph text="-search for movie title to get details, then you can choose if you want to see the spoilers or not ;)" />
+          </div>
         </div>
-        <div className="description">
-          <h4>Making a spoiler request:</h4>
-          <Paragraph text="-Is your desired title not in the search results? don't worry, you can make a request for spoilers by clicking the plus sign on the search box, find your title and make a spoiler request!" />
+        <div className="description-wrapper">
+          <FontAwesomeIcon
+            icon={faPlus}
+            style={{ fontSize: 50 }}
+            className="icon"
+          />
+          <div className="description">
+            <h4>Making a spoiler request:</h4>
+            <Paragraph text="-Is your desired title not in the search results? don't worry, you can make a request for spoilers by clicking the plus sign on the search box, find your title and make a spoiler request!" />
+          </div>
         </div>
-        <div className="description">
-          <h4>Creating account:</h4>
-          <Paragraph text="-By creating an account you agree to sell your soul to us, no of course not. By creating an account you get to add spoilrs, rate other spoilrs and be part of our happy farm. All accounts are private and no user information is shared except username and avatar." />
+
+        <div className="description-wrapper">
+          <FontAwesomeIcon
+            icon={faUserNinja}
+            style={{ fontSize: 50 }}
+            className="icon"
+          />
+          <div className="description">
+            <h4>Creating account:</h4>
+            <Paragraph text="-By creating an account you agree to sell your soul to us, no of course not. By creating an account you get to add spoilrs, rate other spoilrs and be part of our happy farm. All accounts are private and no user information is shared except username and avatar." />
+          </div>
         </div>
-        <div className="description">
-          <h4>Adding spoiler:</h4>
-          <Paragraph text="-Want to add spoilers? You are now our best friend! Just find a title and get to it my friend! Or why not check out our list of spoiler requests to see if you can help someone out. For every spoiler added, you get a shiny point. For every upvote; you guessed it -point! Share your movie knowledge with us and welcome to the spoilr family!" />
+        <div className="description-wrapper">
+          <FontAwesomeIcon
+            icon={faStar}
+            style={{ fontSize: 50 }}
+            className="icon"
+          />
+          <div className="description">
+            <h4>Adding spoiler:</h4>
+            <Paragraph text="-Want to add spoilers? You are now our best friend! Just find a title and get to it my friend! Or why not check out our list of spoiler requests to see if you can help someone out. For every spoiler added, you get a shiny point. For every upvote; you guessed it -point! Share your movie knowledge with us and welcome to the spoilr family!" />
+          </div>
         </div>
       </section>
     </Layout>
