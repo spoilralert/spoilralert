40a692291883770e09ed3f208c4ac02dff9e6100
Linda-404 <Linda-404@users.noreply.github.com>
2022-06-08 13:49:32 +0200

    change logo icon, change fontsize btn

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import HeaderNav from "./HeaderNav";
 import Image from "next/image";
-import Logo from "../public/images/logo_rooster_white1.png";
+// import Logo from "../public/images/logo_rooster_white1.png";
+import LogoIcon from "../public/images/rooster_icon1.png";
 import Search from "./Search";
 import HamburgerMenu from "./HamburgerMenu";
 import Link from "next/link";
@@ -11,7 +12,12 @@ export default function Header() {
       <div className="header__logo">
         <Link href="/">
           <a className="header__logo__link">
-            <Image src={Logo} width="60" height="60" alt="spoilralet logo" />
+            <Image
+              src={LogoIcon}
+              width="62"
+              height="50"
+              alt="spoilralert logo"
+            />
           </a>
         </Link>
       </div>
