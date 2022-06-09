ce9dc1e7359a01749a66d67cf3163af408ce66fa
Linda-404 <Linda-404@users.noreply.github.com>
2022-06-09 13:16:14 +0200

    add meta tag scale

--- components/layout/Head.js
+++ components/layout/Head.js
@@ -4,8 +4,8 @@ export default function Head({ title = "" }) {
   return (
     <NextHead>
       <meta
-        name="veiwport"
-        content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=no;user-scalable=0;"
+        name="viewport"
+        content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1"
       />
       <title>
         {title}
