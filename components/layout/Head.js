665a6f5e3aedba24af58aa0a74ec276d22adfdcb
Linda-404 <Linda-404@users.noreply.github.com>
2022-06-09 13:07:20 +0200

    add meta tag scale

--- components/layout/Head.js
+++ components/layout/Head.js
@@ -3,6 +3,10 @@ import NextHead from "next/head";
 export default function Head({ title = "" }) {
   return (
     <NextHead>
+      <meta
+        name="veiwport"
+        content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=no;user-scalable=0;"
+      />
       <title>
         {title}
         {title ? " | " : ""}spoilralert
