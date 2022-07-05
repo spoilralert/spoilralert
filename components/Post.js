6ce544d8bd16ec13690bd1982ef1561ef94398cd
Linda-404 <Linda-404@users.noreply.github.com>
2022-07-05 23:55:46 +0200

    update successmessage on postform

--- components/Post.js
+++ components/Post.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
-import {UpdateSpoilrVotes} from "../lib/spoilrs";
+import { UpdateSpoilrVotes } from "../lib/spoilrs";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 import Paragraph from "./paragraph";
 import Vote from "./Vote";
@@ -16,32 +18,37 @@ export default function Post({ spoilr }) {
   const handleVote = async (newSum) => {
     setVotes(newSum);
     const data = await UpdateSpoilrVotes(spoilr.id, newSum);
-  }
+  };
 
   return (
     <div className={`card ${expand ? "expand" : ""}`}>
-      <div className="card__content" onClick={handleToggle}>
+      <div className="card__content">
         <div>
-          <h4>{spoilr.attributes.title}</h4>
-          <div className="card__info">
-            <h5>{username}</h5>
-            <h5>{spoilr.attributes.createdAt}</h5>
+          <div className="card__heading">
+            <h4>{spoilr.attributes.title}</h4>
+            {/* <FontAwesomeIcon icon={faXmark} className="delete-icon" /> */}
           </div>
-          <div className={`card__text ${expand ? "expand-text" : ""}`}>
-            <Paragraph text={spoilr.attributes.content} />
+          <div onClick={handleToggle}>
+            <div className="card__info">
+              <h5>{username}</h5>
+              <h5>{spoilr.attributes.createdAt}</h5>
+            </div>
+            <div className={`card__text ${expand ? "expand-text" : ""}`}>
+              <Paragraph text={spoilr.attributes.content} />
+            </div>
           </div>
-        </div>
-        <div className="tags__container">
-          {spoilr.attributes.tags.data.map((tag, i) => (
-            <h5 key={i} className="tag">
-              {tag.attributes.name}
-            </h5>
-          ))}
+          <div className="tags__container">
+            {spoilr.attributes.tags.data.map((tag, i) => (
+              <h5 key={i} className="tag">
+                {tag.attributes.name}
+              </h5>
+            ))}
+          </div>
         </div>
       </div>
 
       <div>
-        <Vote votes={votes} handleVote={handleVote}/>
+        <Vote votes={votes} handleVote={handleVote} />
       </div>
     </div>
   );
